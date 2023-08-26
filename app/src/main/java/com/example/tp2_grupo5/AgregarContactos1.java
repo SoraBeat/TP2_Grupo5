948e49d68533fcd429e1c0a59c07eccd884047fc
MartinaP16 <54824480+MartinaP16@users.noreply.github.com>
2023-08-26 00:52:54 -0300

    Validaciones + Diseño 2do Paso

--- app/src/main/java/com/example/tp2_grupo5/AgregarContactos1.java
+++ app/src/main/java/com/example/tp2_grupo5/AgregarContactos1.java
@@ -5,15 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AgregarContactos1 extends AppCompatActivity {
+
+    private Spinner spinnerTel;
+    private Spinner spinnerEmail;
+    private EditText editTxtEmail;
+    private EditText editTxtDate;
+    private EditText editTxtApellido;
+    private EditText editTxtNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_contactos1);
+
+        editTxtApellido = (EditText)findViewById(R.id.etApellido);
+        editTxtNombre = (EditText)findViewById(R.id.etNombre);
+        editTxtEmail = (EditText)findViewById(R.id.etEmail);
+        editTxtDate = (EditText)findViewById(R.id.etFechaNac);
+
+        spinnerTel = (Spinner)findViewById(R.id.spinnerTelefono);
+        spinnerEmail = (Spinner)findViewById(R.id.spinnerEmail);
+
+        //Opciones del Spinner
+        String[] opciones = {"Casa", "Trabajo", "Móvil"};
+        ArrayAdapter<String> adapterSpinnner = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
+
+        spinnerTel.setAdapter(adapterSpinnner);
+        spinnerEmail.setAdapter(adapterSpinnner);
     }
 
     @Override
@@ -34,5 +67,51 @@ public class AgregarContactos1 extends AppCompatActivity {
             startActivity(i);
         }
         return true;
+    }
+
+    //Controlador del boton continuar
+    public void btnContinuar_Click(View view)
+    {
+        String apellido = editTxtApellido.getText().toString().trim();
+        String nombre = editTxtNombre.getText().toString().trim();
+        String email = editTxtEmail.getText().toString().trim();
+        String fechaNac = editTxtDate.getText().toString().trim();
+
+
+        Pattern formatoSoloLetras = Pattern.compile("[^0-9]*");
+        Matcher matcherApellido = formatoSoloLetras.matcher(apellido);
+        Matcher matcherNombre = formatoSoloLetras.matcher(nombre);
+
+        Pattern fechaNacFormato = Pattern.compile("\\d{2}/\\d{2}/\\d{4}");
+        Matcher matcherFechaNac = fechaNacFormato.matcher(fechaNac);
+
+        //Verifico que el apellido no contenga numeros
+        if(!nombre.isEmpty() && matcherNombre.matches())
+        {
+            //Verifico que el nombre no contenga numeros
+            if(!apellido.isEmpty() && matcherApellido.matches())
+            {
+                //Verifico que sea un mail valido
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!fechaNac.isEmpty() && matcherFechaNac.matches()) //Si la fecha no esta vacia y el formato es valido dd/MM/yyyy
+                    {
+                        Intent viewAgregarContactos2 = new Intent(this, AgregarContactos2.class);
+                        startActivity(viewAgregarContactos2);
+                    } else {
+                        Toast.makeText(this, "La fecha de nacimiento no es valida", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Correo electrónico no válido", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "El apellido no puede quedar vacio o contener números", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "El nombre no puede quedar vacio o contener números", Toast.LENGTH_SHORT).show();
+        }
     }
 }
